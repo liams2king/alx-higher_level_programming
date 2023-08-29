f11418698431b0aed5d9d82ef52369ca54912218
liams2king <nguewa237@icloud.com>
2023-08-28 19:29:07 -0700

    101-safe_function.py

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -2,29 +2,21 @@
 
 import sys
 
+
 def safe_function(fct, *args):
     """Executes a function safely.
 
-     args:
-         fct: The function to execute.
-         args: The arguments for the fct function.
+    Args:
+        fct: The function to execute.
+        args: Arguments for fct.
 
-     Returns:
-         If an error occurs - None.
-         Otherwise - the result of the call to fct.
-     """
+    Returns:
+        If an error occurs - None.
+        Otherwise - the result of the call to fct.
+    """
     try:
         result = fct(*args)
-        return result
-    except (Exception, BaseException) as e:
-        print(f"Exception: {e}", file=sys.stderr)
-        return
-
-def divide(a, b):
-    return a / b
-
-result = safe_function(divide, 10, 0)
-if result is not None:
-    print(f"Result: {result}")
-else:
-    print("Function execution failed.")
+        return (result)
+    except:
+        print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
+        return (None)
